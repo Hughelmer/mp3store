b123da59c20d20e02c51a02a175700fbce926694
julimer.l.b <julimer.l.b@gmail.com>
2023-08-29 09:41:18 +0800

    initial commit

---
+++ views/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Login</title>
+	</head>
+
+	<body>
+		<form action="login_process.php" method="POST">
+			<h2>Login</h2>
+			<input type="text" name="username" placeholder="Username" required><br>
+			<input type="password" name="password" placeholder="Password" required><br>
+			<button type="submit">Login</button>
+		</form>
+	</body>
+</html>
